b15322c3b4daa2bbd6b517fe8e01c42fa66456dd
Alexandros Papagiannakis <alexandros@DN0a237b30.SUNet>
2025-02-06 14:51:46 -0800

    added the correct condition for np.isnan for float numpy objects

--- polygon_selection.py
+++ polygon_selection.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import pickle_read_save as prs
 from scipy.stats import gaussian_kde
 import shapely.geometry as shape
+import pandas as pd
 
 # Source: https://matplotlib.org/stable/gallery/widgets/polygon_selector_demo.html
 class SelectFromCollection:
@@ -69,10 +70,30 @@ def density_scatter(x,y):
     return z
 
 
-def return_markers_in_polygon(x, y, x_variable, y_variable, gate_name, colormap):
+def remove_nans_and_infs(x,y, sample_size):
+    
+    gate_df = pd.DataFrame()
+    gate_df['x'] = x
+    gate_df['y'] = y
+    gate_df.replace([np.inf, -np.inf], np.nan, inplace=True)
+    gate_df.dropna(inplace=True)
+    
+    gate_df = gate_df.sample(sample_size)
+    
+    x = gate_df.x
+    y = gate_df.y
+    
+    return x, y 
+
+
+def return_markers_in_polygon(x, y, x_variable, y_variable, gate_name, sample_size, colormap):
+    
     fig, ax = plt.subplots(figsize=(6,6))
+    
+    x, y = remove_nans_and_infs(x,y, sample_size)
+    
     z = density_scatter(x,y)
-    pts = ax.scatter(x, y, c=z, cmap=colormap, s=5)
+    pts = ax.scatter(x, y, c=z, cmap=colormap, s=2)
     ax.set_xlabel(x_variable)
     ax.set_ylabel(y_variable)
     
@@ -93,23 +114,20 @@ def return_markers_in_polygon(x, y, x_variable, y_variable, gate_name, colormap)
     print(selector.ind)
     
     polygon_verts = selector.poly.verts
-    gated_data = selector.xys[selector.ind].tolist()
-    gated_data.extend(polygon_verts)  # Add the polygon vertices as a list
-    gated_data.append('polygon_gate')
-    gated_data = gated_data[1:]
-    
+    # gated_data = selector.xys[selector.ind].tolist()
+    # gated_data.extend(polygon_verts)  # Add the polygon vertices as a list
+    gated_data = [polygon_verts, 'polygon_gate']
     prs.save_data(gated_data, gate_name)
     
 
 def get_markers_inside_gate(x,y, polygon):
-    point = get_point_from_coordinates(x,y)
-    if polygon.contains(point):
-        return 1
+    if np.isnan(x) or np.isnan(y):
+        return np.nan
     else:
-        return 0
-
-def get_point_from_coordinates(x,y):
-    return shape.Point(x,y)
+        if polygon.contains(shape.Point(x,y)):
+            return 1
+        else:
+            return 0
 
 def get_polygon_from_coordinates(coord_list):
     return shape.Polygon(coord_list)
