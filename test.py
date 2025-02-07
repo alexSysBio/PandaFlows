eb8c1a2f503ba6268210812c7b308d10aa472011
Alexandros Papagiannakis <51793306+alexSysBio@users.noreply.github.com>
2025-02-06 17:54:28 -0800

    Update test.py

--- test.py
+++ test.py
@@ -5,30 +5,22 @@ import flow_cytometry_class as flowc
 import pickle_read_save as prs
 import numpy as np
 
-fc_data_ceph = '/Users/alexandros/Downloads/02042025_CJW7323_M9glyCAAT_Ceph50.fcs'
-# fc_data_nont = '/Users/alexandros/Downloads/02042025_CJW7323_M9glyCAAT_exp.fcs'
-
-# fn = flowc.flow_cytometry_class(fc_data_nont, 'none')
-fc = flowc.flow_cytometry_class(fc_data_ceph, 'none', '02042025_CJW7323_M9glyCAAT_Ceph50')
-
-# sh.return_selected_ranges(plot_df['RL1-A'], np.arange(0,450,2), 'RL1-A')
-# fcs_df = fn.get_flow_cytometry_dataframe()
-# print(fcs_df.describe())
-
-# fn.histogram_gate('RL2-A', log=False, bin_array=np.arange(0,6000,1))
+fc_data_ceph = '.fcs'
+fc = flowc.flow_cytometry_class(fc_data_ceph, 'none', 'experiment_id')
+# get the Pandas dataframe
 fcs_df = fc.get_flow_cytometry_dataframe()
 print(fcs_df.describe())
-
+# apply histogram gate with log-transformed data
 fc.histogram_gate('RL2-A', log=True, bin_array=np.arange(0, 5, 0.001))
 fcs_df = fc.get_flow_cytometry_dataframe()
 print(fcs_df.describe())
-
+# remove all gates
 fc.reset_gates()
-
+# apply polygon gate to log-transgormed scatter data
 x_variable = 'SSC-A'
 y_variable = 'RL2-A'
 fc.scatter_gate(x_variable, y_variable, True, True, 10000)
 stored_gates = prs.load_data('stored_gates_02042025_CJW7323_M9glyCAAT_Ceph50')
-
+# reload Pandas dataframe and describe the stored data and gates
 fcs_df = fc.get_flow_cytometry_dataframe()
 print(fcs_df.describe())
